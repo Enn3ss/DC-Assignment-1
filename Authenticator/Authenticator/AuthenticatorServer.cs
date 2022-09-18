d1380d340f1cebde19c4fc00cc8923c263b539ac
Enn3ss <110088031+Enn3ss@users.noreply.github.com>
2022-09-18 16:28:59 +0800

    Mostly Finished AuthenticatorServer Implmentation

--- Authenticator/Authenticator/AuthenticatorServer.cs
+++ Authenticator/Authenticator/AuthenticatorServer.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
 
 namespace Authenticator
 {
@@ -13,16 +15,17 @@ namespace Authenticator
     {
         public string Register(string name, string password)
         {
-            string file = @"C:\Users\Nathan Sutandi\Documents\GitHub\DC-Assignment-1\test.txt";
+            string loginFile = @"C:\Users\Nathan Sutandi\Documents\GitHub\DC-Assignment-1\login.txt";
+            // string loginFile = *PUT YOUR FILE PATH HERE AND COMMENT MINE OUT*
             string success = "successfully registered";
 
             try
             {
-                File.WriteAllText(file, name + "," + password);
+                File.AppendAllText(loginFile, name + "," + password + Environment.NewLine); // Every login is added on a new line
             }
-            catch(IOException ex)
+            catch(IOException ex) // Error in opening file
             {
-                Console.WriteLine("Error info:" + ex.Message);
+                Console.WriteLine("Error: " + ex.Message);
                 success = "unsuccessfully registered";
             }
 
@@ -31,13 +34,48 @@ namespace Authenticator
 
         public int Login(string name, string password)
         {
-            return 0;
+            string loginFile = @"C:\Users\Nathan Sutandi\Documents\GitHub\DC-Assignment-1\login.txt";
+            // string loginFile = *PUT YOUR FILE PATH HERE AND COMMENT MINE OUT*
+            string tokenFile = @"C:\Users\Nathan Sutandi\Documents\GitHub\DC-Assignment-1\token.txt";
+            // string tokenFile = *PUT YOUR FILE PATH HERE AND COMMENT MINE OUT*
+            string[] lines = File.ReadLines(loginFile).ToArray();
+
+            foreach (string line in lines)
+            {
+                if(line.Equals(name + "," + password))
+                {
+                    Random rnd = new Random();
+                    int num = rnd.Next();
+                    File.AppendAllText(tokenFile, num.ToString() + Environment.NewLine);
+                    return num; // Return token if name and password match is found in login.txt
+                }
+            }
+            return -1; // Return -1 if name and password cannot be found
         }
 
 
         public string Validate(int token)
         {
-            return "";
+            string tokenFile = @"C:\Users\Nathan Sutandi\Documents\GitHub\DC-Assignment-1\token.txt";
+            // string tokenFile = *PUT YOUR FILE PATH HERE AND COMMENT MINE OUT*
+            string[] lines = File.ReadLines(tokenFile).ToArray();
+
+            foreach (string line in lines)
+            {
+                if(line.Equals(token.ToString()))
+                {
+                    return "validated";
+                }
+            }
+
+            return "not validated";
+        }
+
+        public void ClearSavedTokens() // This function needs to clear saved tokens every 'x' minutes
+        {
+            string tokenFile = @"C:\Users\Nathan Sutandi\Documents\GitHub\DC-Assignment-1\token.txt";
+            // string tokenFile = *PUT YOUR FILE PATH HERE AND COMMENT MINE OUT*
+            File.WriteAllText(tokenFile, String.Empty);
         }
     }
 }
--- Authenticator/Authenticator/RemotingServer.cs
+++ Authenticator/Authenticator/RemotingServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
@@ -11,6 +12,20 @@ namespace Authenticator
     {
         static void Main(string[] args)
         {
+            AuthenticatorServer server = new AuthenticatorServer();
+            /*
+            String success = server.Register("test33", "pass44");
+            String success2 = server.Register("steber", "ringo");
+
+            Console.WriteLine(success);
+            Console.WriteLine(success2);
+            */
+
+            //int token = server.Login("steber1", "ringo");
+            //Console.WriteLine(token);
+
+            string line = server.Validate(90179164);
+            Console.WriteLine(line);
         }
     }
 }
