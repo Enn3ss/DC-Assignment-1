303ae1d38153f7be6cfeffc5ea22f599811a6b1c
Enn3ss <110088031+Enn3ss@users.noreply.github.com>
2022-09-18 15:43:13 +0800

    Initial Commit
    Started Authenticator Project.

---
+++ Authenticator/Authenticator/AuthenticatorServer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authenticator
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext = false)]
+    internal class AuthenticatorServer : IAuthenticatorServer
+    {
+        public string Register(string name, string password)
+        {
+            string file = @"C:\Users\Nathan Sutandi\Documents\GitHub\DC-Assignment-1\test.txt";
+            string success = "successfully registered";
+
+            try
+            {
+                File.WriteAllText(file, name + "," + password);
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine("Error info:" + ex.Message);
+                success = "unsuccessfully registered";
+            }
+
+            return success;
+        }
+
+        public int Login(string name, string password)
+        {
+            return 0;
+        }
+
+
+        public string Validate(int token)
+        {
+            return "";
+        }
+    }
+}
