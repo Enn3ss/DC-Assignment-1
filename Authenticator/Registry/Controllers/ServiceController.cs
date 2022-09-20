1079ba9d7b03b4951b19e425a7a6ed05daca6dc8
Enn3ss <110088031+Enn3ss@users.noreply.github.com>
2022-09-20 10:31:41 +0800

    Added authentication to Registry methods

--- Authenticator/Registry/Controllers/ServiceController.cs
+++ Authenticator/Registry/Controllers/ServiceController.cs
@@ -1,23 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.ServiceModel;
 using System.Web.Http;
 using Newtonsoft.Json;
 using Registry.Models;
+using ServiceProvider.Models;
 
 namespace Registry.Controllers
 {
     [RoutePrefix("api/service")]
     public class ServiceController : ApiController
     {
+        private readonly Authenticator.IAuthenticatorServer foob;
+        
+        public ServiceController()
+        {
+            ChannelFactory<Authenticator.IAuthenticatorServer> foobFactory;
+            NetTcpBinding tcp = new NetTcpBinding();
+            string URL = "net.tcp://localhost/AuthenticationService";
+            foobFactory = new ChannelFactory<Authenticator.IAuthenticatorServer>(tcp, URL);
+            foob = foobFactory.CreateChannel();
+        }
+
         [Route("publish")]
         [HttpPost]
-        public string Publish(List<ServiceDescription> dataList) // Saves service description into description.txt
+        public string Publish(List<ServiceDescription> dataList, int token) // Saves service description into description.txt
         {
             string descriptionFilePath = @"C:\Users\Nathan Sutandi\Documents\GitHub\DC-Assignment-1\description.txt";
             // string descriptionFilePath = *PUT YOUR FILE PATH HERE AND COMMENT MINE OUT*
             string jsonContent;
             StatusData status;
+
+            if (foob.Validate(token).Equals("not validated")) // If token invalid return denied status
+            {
+                ErrorData error = new ErrorData
+                {
+                    Status = "Denied",
+                    Reason = "Authentication Error"
+                };
+
+                return JsonConvert.SerializeObject(error, Formatting.Indented);
+            }
 
             try
             {
@@ -48,59 +72,114 @@ namespace Registry.Controllers
 
         [Route("search")]
         [HttpPost]
-        public string Search(string search) // Returns service info based on 'search' string
+        public string Search(string search, int token) // Returns service info based on 'search' string
         {
             string descriptionFilePath = @"C:\Users\Nathan Sutandi\Documents\GitHub\DC-Assignment-1\description.txt";
             // string descriptionFilePath = *PUT YOUR FILE PATH HERE AND COMMENT MINE OUT*
-            string jsonContent = File.ReadAllText(descriptionFilePath);
-            List<ServiceDescription> dataList = JsonConvert.DeserializeObject<List<ServiceDescription>>(jsonContent); // Deserializing jsonContent into a collection
-            List<ServiceDescription> newDataList = new List<ServiceDescription>();
+            string jsonContent;
+            List<ServiceDescription> dataList;
+            List<ServiceDescription> newDataList;
 
-            foreach (ServiceDescription data in dataList) // Searches collection for service info that matches 'search'
+            if (foob.Validate(token).Equals("validated"))
             {
-                if(data.Name.IndexOf(search, StringComparison.OrdinalIgnoreCase) >= 0 ||
-                   data.Description.IndexOf(search, StringComparison.OrdinalIgnoreCase) >= 0)
+                jsonContent = File.ReadAllText(descriptionFilePath);
+                dataList = JsonConvert.DeserializeObject<List<ServiceDescription>>(jsonContent); // Deserializing jsonContent into a collection
+                newDataList = new List<ServiceDescription>();
+
+                foreach (ServiceDescription data in dataList) // Searches collection for service info that matches 'search'
                 {
-                    newDataList.Add(data);
+                    if (data.Name.IndexOf(search, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                        data.Description.IndexOf(search, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        newDataList.Add(data);
+                    }
                 }
+
+                jsonContent = JsonConvert.SerializeObject(newDataList.ToArray(), Formatting.Indented); // Return matches
+            }
+            else // Else if token is invalid
+            {
+                ErrorData error = new ErrorData
+                {
+                    Status = "Denied",
+                    Reason = "Authentication Error"
+                };
+
+                jsonContent = JsonConvert.SerializeObject(error, Formatting.Indented); // Return denied status
             }
 
-            return JsonConvert.SerializeObject(newDataList.ToArray(), Formatting.Indented);
+            return jsonContent;
         }
 
         [Route("allservices")]
         [HttpPost]
-        public string AllServices() // Returns all services saved in description.txt in JSON format
+        public string AllServices(int token) // Returns all services saved in description.txt in JSON format
         {
             string descriptionFilePath = @"C:\Users\Nathan Sutandi\Documents\GitHub\DC-Assignment-1\description.txt";
             // string descriptionFilePath = *PUT YOUR FILE PATH HERE AND COMMENT MINE OUT*
-            //string jsonContent = File.ReadAllText(descriptionFilePath);
-            //List<ServiceDescription> dataList = JsonConvert.DeserializeObject<List<ServiceDescription>>(jsonContent); // Deserializing jsonContent into a collection
+            string jsonContent;
 
-            //return JsonConvert.SerializeObject(dataList.ToArray(), Formatting.Indented);
-            return File.ReadAllText(descriptionFilePath);
+            if (foob.Validate(token).Equals("validated")) // If token is valid (found in token.txt)
+            {            
+                jsonContent = File.ReadAllText(descriptionFilePath); // Return all services
+            }
+            else // Else if token is invalid
+            {
+                ErrorData error = new ErrorData
+                {
+                    Status = "Denied",
+                    Reason = "Authentication Error"
+                };
+
+                jsonContent = JsonConvert.SerializeObject(error, Formatting.Indented); // Return denied status
+            }
+
+            return jsonContent;
         }
 
         [Route("unpublish")]
         [HttpPost]
-        public void Unpublish(string serviceEndpoint)
+        public string Unpublish(string serviceEndpoint, int token) // Given a serviceEndpoint, remove the service description from description.txt
         {
             string descriptionFilePath = @"C:\Users\Nathan Sutandi\Documents\GitHub\DC-Assignment-1\description.txt";
             // string descriptionFilePath = *PUT YOUR FILE PATH HERE AND COMMENT MINE OUT*
-            string jsonContent = File.ReadAllText(descriptionFilePath);
-            List<ServiceDescription> dataList = JsonConvert.DeserializeObject<List<ServiceDescription>>(jsonContent); // Deserializing jsonContent into a collection
-            List<ServiceDescription> newDataList = new List<ServiceDescription>();
+            string jsonContent;
+            string status;
+            List<ServiceDescription> dataList;
+            List<ServiceDescription> newDataList;
+            ErrorData error = new ErrorData();
 
-            foreach (ServiceDescription data in dataList)
+            if (foob.Validate(token).Equals("validated")) // If token is valid (found in token.txt)
             {
-                if (!data.APIEndpoint.Equals(serviceEndpoint)) // If serviceEndpoint matches APIEndpoint then do not add to new list
+                jsonContent = File.ReadAllText(descriptionFilePath);
+                dataList = JsonConvert.DeserializeObject<List<ServiceDescription>>(jsonContent); // Deserializing jsonContent into a list
+                newDataList = new List<ServiceDescription>();
+
+                foreach (ServiceDescription data in dataList)
                 {
-                    newDataList.Add(data);
+                    if (!data.APIEndpoint.Equals(serviceEndpoint)) // If serviceEndpoint matches APIEndpoint then do not add to new list
+                    {
+                        newDataList.Add(data);
+                    }
                 }
+
+                jsonContent = JsonConvert.SerializeObject(newDataList.ToArray(), Formatting.Indented); // Serialize jsonContent into new list
+                File.WriteAllText(descriptionFilePath, jsonContent); // Rewrite description.txt with newDataList in JSON format
+
+                error.Status = "Approved";
+                error.Reason = "Authentication Authorised";
+
+                status = JsonConvert.SerializeObject(error, Formatting.Indented);
+            }
+            else // Else if token is invalid
+            {
+                error.Status = "Denied";
+                error.Reason = "Authentication Error";
+
+                status = JsonConvert.SerializeObject(error, Formatting.Indented);
             }
 
-            jsonContent = JsonConvert.SerializeObject(newDataList.ToArray(), Formatting.Indented);
-            File.WriteAllText(descriptionFilePath, jsonContent); // Rewrite description.txt with newDataList in JSON format
+            return status;
         }
     }
 }
---
+++ Authenticator/Registry/Models/ErrorData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceProvider.Models
+{
+    public class ErrorData
+    {
+        public string Status { get;set; }
+        public string Reason { get;set; }
+    }
+}
